f074dc46884a32fc22f5411817c119880f116a29
Nicolas Schäfli <nschaefli@Nicolass-MacBook-Pro.local>
2024-12-19 21:21:45 +0100

    Init commmit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use crate::day1::day1_1;
+use crate::day3::day3_2;
+use crate::day3::day3_1;
+use crate::day4::day4_1;
+
+mod day3;
+mod day4;
+mod day1;
+
+fn main() {
+    println!("Hello, world!");
+    day1_1();
+    //day3_1();
+    //day3_2();
+    //day4_1();
+}
